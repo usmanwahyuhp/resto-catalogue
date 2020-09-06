bd13e6beec061deb0c8fd1ce52d80c8e5f28cbc0
usman <usmanwahyuhp@gmail.com>
2020-09-06 21:31:44 +0700

    submit rev 1

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -4,25 +4,22 @@ console.log("data");
 console.log(data.restaurants[0]);
 
 let Card = "";
-let i = 0;
-while (i < 3) {
-  console.log(data.restaurants[i].id);
-  //   let data = data.restaurants[i];
+
+data.restaurants.forEach((resto) => {
   Card += `
           <li class="cards_item">
               <div class="card">
                   <div class="card_image">
-                      <img src="${data.restaurants[i].pictureId}" alt="resto-img"/>
-                  </div>
+                      <img src="${resto.pictureId}" alt="${resto.name}"/>
+                      </div>
                   <div class="card_content">
-                      <h4 class="card_title">Rating: ${data.restaurants[i].rating}</h4>
-                      <h2 class="card_title">${data.restaurants[i].name} - ${data.restaurants[i].city}</h2>
-                      <p class="card_text">${data.restaurants[i].description}</p>
+                      <h4 class="card_title">Rating: ${resto.rating}</h4>
+                      <h2 class="card_title">${resto.name} - ${resto.city}</h2>
+                      <p class="card_text">${resto.description}</p>
                       <button class="btn card_btn">Read More</button>
                   </div>
               </div>
           </li>
   `;
   document.getElementById("cards").innerHTML = Card;
-  i++;
-}
+});
